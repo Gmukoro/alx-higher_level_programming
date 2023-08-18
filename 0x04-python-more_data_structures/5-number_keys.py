eab2708210e2680632fd51c108cf8782114eb002
Gmukoro <gmukoro3@gmail.com>
2023-08-18 22:43:48 +0000

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def number_keys_dictionary):
-    number = 0
+def number_keys(a_dictionary):
+    numb = 0
     list_keys = list(a_dictionary.keys())
-    for t in list_keys:
-        number += 1
-    return (number)
+    for i in list_keys:
+        numb += 1
+    return (numb)
