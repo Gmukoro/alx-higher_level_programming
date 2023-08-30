bc2cb51cb526babe2b4998d015a37682311b8014
Gmukoro <gmukoro3@gmail.com>
2023-08-29 19:35:39 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Define a class Sq."""
+
+
+class Square:
+    """Represent a sq."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new sq.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the sq."""
+        return (self.__size * self
