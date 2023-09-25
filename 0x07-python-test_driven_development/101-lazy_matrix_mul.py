71e41a978cca0ce969abd47fcf987da0e7aabe8c
Gmukoro <gmukoro3@gmail.com>
2023-09-25 03:28:29 +0000

    101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-""" My add_integer module """
+""" Numpy to multiplies two matrices the lazy way"""
+import numpy as np
 
 
-def add_integer(a, b=98):
-    """Function that returns the addition of a + b
+def lazy_matrix_mul(m_a, m_b):
+        """ Function that takes in 2 matrices and multiplies them
 
-    Args:
-        a: should be an int. if not throw error
-        b: second int. if not throw error. default val is 98.
+        Args:
+            m_a: first matrix
+            m_b: second matrix
 
-    Returns:
-        The addition of a + b or a raised TypeError
-    """
-    if type(a) is int or type(a) is float:
-        if type(b) is int or type(b) is float:
-            return int(a + b)
-        else:
-            raise TypeError("b must be an integer")
-    else:
-        raise TypeError("a must be an integer")
+        Return: the product of m_a and m_b
+        """
+        return np.matmul(m_a, m_b)
