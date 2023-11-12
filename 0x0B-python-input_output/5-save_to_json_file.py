ad0c471ba1c73438d882c654b95bdf76dd76da96
Gmukoro <gmukoro3@gmail.com>
2023-11-12 14:50:02 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/5-save_to_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a JSON file-writing function."""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """write the object to the file in JSON rep"""
-    with open(filename, 'w') as t:
-        json.dump(my_obj, t)
