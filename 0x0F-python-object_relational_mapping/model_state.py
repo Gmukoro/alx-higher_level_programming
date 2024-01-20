aff06adc0e578334de3eab420df3ed7c8d1c422c
Gmukoro <gmukoro3@gmail.com>
2024-01-20 00:47:17 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
+Module that contains the class definition of a State and an instance Base = declarative_base().
+"""
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class:
+    - inherits from Base
+    - links to the MySQL table states
+    - class attribute id represents a column of an auto-generated, unique integer, can’t be null, and is a primary key
+    - class attribute name represents a column of a string with a maximum of 128 characters and can’t be null
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
