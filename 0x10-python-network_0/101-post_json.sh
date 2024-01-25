b8eceb8cd054f763797d957ab7057475e501cbd1
Gmukoro <gmukoro3@gmail.com>
2024-01-25 19:03:17 +0000

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-# JSON POST request to a URL passed as the first arg and displays the body of the response
-curl -s -H "Content-Type: application/json" -d @"$2" "$1".
+
+# Check if the number of arguments is not equal to 2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 URL JSON_FILE"
+    exit 1
+fi
+
+# Extract arguments
+url=$1
+json_file=$2
+
+# Check if the JSON file exists
+if [ ! -f "$json_file" ]; then
+    echo "Error: JSON file '$json_file' not found."
+    exit 1
+fi
+
+# Send POST request with JSON content and display response body
+curl -s -X POST -H "Content-Type: application/json" -d @"$json_file" "$url"
+
